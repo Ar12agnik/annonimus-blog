eb0e0cb21b05a012c35a58019d30e31a56138fbb
Agnik Roy <102238499+Ar12agnik@users.noreply.github.com>
2024-01-18 23:15:47 +0530

    upload

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,3 @@ class posts(models.Model):
     content=models.CharField(max_length=300)
     pub_date =models.DateField(auto_now_add=True)
     pub_time = models.TimeField(auto_now_add=True)
-    def save(self, *args, **kwargs):
-        # Set pub_time to the current time if pub_date is not provided
-        if not self.pub_date:
-            self.pub_date = datetime.now()
-        self.pub_time = self.pub_date.time()
-        super().save(*args, **kwargs)
